6fbb1ed37726cea71f93b6cd0aa17f371d685450
Muh. Awaluddin <155923104+muhwal21@users.noreply.github.com>
2024-09-27 02:19:38 +0700

    Update sri.js

--- sri.js
+++ sri.js
@@ -1,11 +1,11 @@
-let accessToken = ""; 
+let accessToken = "";
 
 const uploadUrl = "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart";
-const folderId = "1lkq3UdLDd9-aS8J7WrPUCkyJmLscB3HQ"; 
+const folderId = "1lkq3UdLDd9-aS8J7WrPUCkyJmLscB3HQ";
 
 document.getElementById("uploadButton").addEventListener("click", async () => {
     if (!accessToken) {
-        authenticate(); 
+        authenticate();
     } else {
         const fileInput = document.getElementById("fileUpload");
         const file = fileInput.files[0];
@@ -14,7 +14,7 @@ document.getElementById("uploadButton").addEventListener("click", async () => {
                 await uploadFileToDrive(file);
             } catch (error) {
                 console.error("File upload failed:", error);
-                alert("Error uploading file. Please try again.");
+                alert(`Error uploading file: ${error.message}. Please try again.`);
             }
         } else {
             alert("Please select a file.");
@@ -40,18 +40,17 @@ async function uploadFileToDrive(file) {
     });
 
     if (!response.ok) {
-        throw new Error("File upload failed. Status: " + response.status);
+        throw new Error(`File upload failed. Status: ${response.status}`);
     }
 
     const data = await response.json();
     console.log("File uploaded successfully:", data);
     alert("File uploaded successfully!");
-    // Lakukan update izin di sini jika diperlukan
 }
 
 function authenticate() {
-    const clientId = "21812121373-ehtg2sccmgirt697mtmu37kmcs0dgs9c.apps.googleusercontent.com"; 
-    const redirectUri = "https://testgoogle-drive.vercel.app"; 
+    const clientId = "21812121373-ehtg2sccmgirt697mtmu37kmcs0dgs9c.apps.googleusercontent.com";
+    const redirectUri = "https://testgoogle-drive.vercel.app";
     const scope = "https://www.googleapis.com/auth/drive.file";
     const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=token`;
 
@@ -63,7 +62,7 @@ function handleAuthToken() {
     if (params.has("access_token")) {
         accessToken = params.get("access_token");
         console.log("Access Token found:", accessToken);
-        window.history.pushState({}, document.title, window.location.pathname); 
+        window.history.pushState({}, document.title, window.location.pathname);
     }
 }
 
